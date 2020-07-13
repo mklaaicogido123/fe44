9872e20edc8fa32dfb3431805071e94892be68eb
nguyen duy phong <mklaaicogido123@gmail.com>
2020-07-13 21:29:58 +0700

    doi mau xe

--- src/HandleState/ChonXe.js
+++ src/HandleState/ChonXe.js
@@ -1,6 +1,16 @@
 import React, { Component } from 'react'
 
 export default class ChonXe extends Component {
+    
+    state={
+        srcImg:'./img/red-car.jpg'
+    }
+    changeColor = (newImgSrc) =>{
+        this.setState({
+            srcImg:newImgSrc
+        })
+    }
+
     render() {
         return (
             <div>
@@ -8,23 +18,30 @@ export default class ChonXe extends Component {
                     <h3 className="text-center">bài tập chọn xe</h3>
                     <div className="row">
                         <div className="col-6">
-                            <img className="w-100" src={'./img/red-car.jpg'} alt='red_car.jpg'></img>
+                            <img className="w-100" src={this.state.srcImg} alt='red_car.jpg'></img>
                         </div>
                         <div className="col-6">
                             <div className="row">
                                 <div className="col-3">
-                                    <button className="w-50 h-100 btn btn-outline-dark bg-danger text-light">red</button>
+                                    <button className="w-50 h-100 btn btn-outline-dark bg-danger text-light"
+                                    onClick={() =>
+                                        this.changeColor('./img/red-car.jpg') }>red</button>
                                 </div>
                                 <div className="col-3">
-                                    <button className="w-50 h-100 btn btn-outline-dark bg-danger text-light">black</button>
+                                    <button className="w-50 h-100 btn btn-outline-dark bg-danger text-light"
+                                    onClick={() =>
+                                        this.changeColor('./img/black-car.jpg') }>black</button>
                                 </div>
                                 <div className="col-3">
-                                    <button className="w-50 h-100 btn btn-outline-dark bg-danger text-light">silver</button>
+                                    <button className="w-50 h-100 btn btn-outline-dark bg-danger text-light"
+                                     onClick={() =>
+                                     this.changeColor('./img/silver-car.jpg') }>silver</button>
                                 </div>
                                 <div className="col-3">
-                                    <button className="w-50 h-100 btn btn-outline-dark bg-danger text-light">Steel</button>
-                                </div>
-                                
+                                    <button className="w-50 h-100 btn btn-outline-dark bg-danger text-light"
+                                    onClick={() =>
+                                        this.changeColor('./img/steel-car.jpg') }>Steel</button>
+                                </div>                              
                             </div>
                         </div>
                     </div>
